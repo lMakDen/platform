04540d319ebba11a268c564798762f641e456590
Ivan <MakDen1994@gmail.com>
2017-09-17 11:36:13 +0300

    base version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-
+import App from './containers/App';
 import './styles/app.css'
 import configureStore from './store/configureStore'
 
@@ -10,8 +10,7 @@ const store = configureStore();
 render(
   <Provider store={store}>
     <div className='app'>
-        <div>Hello</div>
-      {/*<App />*/}
+      <App />
     </div>
   </Provider>,
   document.getElementById('root')
